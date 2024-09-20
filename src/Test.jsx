fed5117e1a0e7466b2853d3cd68f12a9b8ef590c
Arthur Mondon <arthur-mdn@users.noreply.github.com>
2024-09-20 11:42:29 +0200

    Add airdop function to credit wallet

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,8 +1,11 @@
 import { useState, useEffect } from 'react';
+import { Connection, clusterApiUrl, PublicKey, LAMPORTS_PER_SOL } from '@solana/web3.js';
 
 const MintNFTButton = () => {
     const [walletConnected, setWalletConnected] = useState(false);
     const [walletAddress, setWalletAddress] = useState('');
+    const [provider, setProvider] = useState(null);
+    const [loading, setLoading] = useState(false);
 
     const checkIfWalletIsConnected = async () => {
         if (window.solana && window.solana.isPhantom) {
@@ -10,6 +13,7 @@ const MintNFTButton = () => {
                 const response = await window.solana.connect({ onlyIfTrusted: true });
                 setWalletConnected(true);
                 setWalletAddress(response.publicKey.toString());
+                setProvider(window.solana);
             } catch (err) {
                 console.error('Erreur de connexion au portefeuille:', err);
             }
@@ -23,6 +27,7 @@ const MintNFTButton = () => {
             const response = await window.solana.connect();
             setWalletConnected(true);
             setWalletAddress(response.publicKey.toString());
+            setProvider(window.solana);
         } catch (err) {
             console.error('Erreur de connexion au portefeuille:', err);
         }
@@ -31,6 +36,26 @@ const MintNFTButton = () => {
     useEffect(() => {
         checkIfWalletIsConnected();
     }, []);
+
+    const airDropHelper = async () => {
+        try {
+            setLoading(true);
+            console.log("Airdrop en cours...");
+            const connection = new Connection(clusterApiUrl("testnet"), "confirmed");
+            const fromAirDropSignature = await connection.requestAirdrop(
+                new PublicKey(walletAddress),
+                LAMPORTS_PER_SOL
+            );
+            await connection.confirmTransaction(fromAirDropSignature, { commitment: "confirmed" });
+
+            console.log(`1 SOL a été airdroppé sur votre portefeuille ${walletAddress} avec succès`);
+        } catch (err) {
+            console.error("Erreur lors de l'Airdrop:", err);
+        } finally {
+            console.log("Réinitialisation du statut de chargement");
+            setLoading(false);
+        }
+    };
 
     return (
         <div style={{ textAlign: 'center', marginTop: '50px' }}>
@@ -51,6 +76,22 @@ const MintNFTButton = () => {
                     >
                         Mint NFT
                     </button>
+                    <p>Airdrop 1 SOL
+                        <button
+                            disabled={loading}
+                            onClick={airDropHelper}
+                            style={{
+                                padding: '10px 20px',
+                                backgroundColor: '#4CAF50',
+                                color: 'white',
+                                border: 'none',
+                                borderRadius: '5px',
+                                cursor: 'pointer',
+                            }}
+                        >
+                            {loading ? 'Airdrop en cours...' : 'AirDrop SOL'}
+                        </button>
+                    </p>
                 </>
             ) : (
                 <button
