8349595b02e1e89215dc1b401d902a1c0fb4bbe8
Arthur Mondon <arthur-mdn@users.noreply.github.com>
2024-09-22 09:46:46 +0200

    Check balance button

--- src/MintNFTButton.jsx
+++ src/MintNFTButton.jsx
@@ -1,16 +1,19 @@
-import { useState, useEffect } from 'react';
-import { Connection, clusterApiUrl, PublicKey, LAMPORTS_PER_SOL } from '@solana/web3.js';
+import {Fragment, useEffect, useState} from 'react';
+import axios from 'axios';
+import {clusterApiUrl, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey, Transaction} from '@solana/web3.js';
+import {getOrCreateAssociatedTokenAccount, mintTo} from '@solana/spl-token';
 
 const MintNFTButton = () => {
     const [walletConnected, setWalletConnected] = useState(false);
     const [walletAddress, setWalletAddress] = useState('');
     const [provider, setProvider] = useState(null);
     const [loading, setLoading] = useState(false);
+    const [balance, setBalance] = useState(null);
 
     const checkIfWalletIsConnected = async () => {
         if (window.solana && window.solana.isPhantom) {
             try {
-                const response = await window.solana.connect({ onlyIfTrusted: true });
+                const response = await window.solana.connect({onlyIfTrusted: true});
                 setWalletConnected(true);
                 setWalletAddress(response.publicKey.toString());
                 setProvider(window.solana);
@@ -41,68 +44,62 @@ const MintNFTButton = () => {
         try {
             setLoading(true);
             console.log("Airdrop en cours...");
-            const connection = new Connection(clusterApiUrl("testnet"), "confirmed");
+            const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
             const fromAirDropSignature = await connection.requestAirdrop(
                 new PublicKey(walletAddress),
                 LAMPORTS_PER_SOL
             );
-            await connection.confirmTransaction(fromAirDropSignature, { commitment: "confirmed" });
-
+            await connection.confirmTransaction(fromAirDropSignature, {commitment: "confirmed"});
             console.log(`1 SOL a été airdroppé sur votre portefeuille ${walletAddress} avec succès`);
         } catch (err) {
             console.error("Erreur lors de l'Airdrop:", err);
         } finally {
-            console.log("Réinitialisation du statut de chargement");
             setLoading(false);
         }
     };
 
+    const checkBalance = async () => {
+        try {
+            setLoading(true);
+            const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+            const balance = await connection.getBalance(new PublicKey(walletAddress));
+            console.log('Solde du portefeuille:', balance / LAMPORTS_PER_SOL);
+            setBalance(balance / LAMPORTS_PER_SOL);
+        } catch (error) {
+            console.error('Erreur lors de la vérification du solde:', error);
+        } finally {
+            setLoading(false);
+        }
+    }
+
     return (
-        <div style={{ textAlign: 'center', marginTop: '50px' }}>
+        <div style={{textAlign: 'center', marginTop: '50px'}}>
             <h2>Mint NFT sur Solana</h2>
             {walletConnected ? (
                 <>
                     <p>Portefeuille connecté: {walletAddress}</p>
                     <button
-                        style={{
-                            padding: '10px 20px',
-                            backgroundColor: '#4CAF50',
-                            color: 'white',
-                            border: 'none',
-                            borderRadius: '5px',
-                            cursor: 'pointer',
-                        }}
-                        onClick={() => alert('Fonctionnalité de Mint à implémenter !')}
+                        type={'button'}
+                        disabled={loading}
+                        onClick={airDropHelper}
                     >
-                        Mint NFT
+                        Airdrop 1 SOL
                     </button>
-                    <p>Airdrop 1 SOL
+                    <p>
+                        {balance && <span>{balance} SOL</span>}
                         <button
+                            type={'button'}
                             disabled={loading}
-                            onClick={airDropHelper}
-                            style={{
-                                padding: '10px 20px',
-                                backgroundColor: '#4CAF50',
-                                color: 'white',
-                                border: 'none',
-                                borderRadius: '5px',
-                                cursor: 'pointer',
-                            }}
+                            onClick={checkBalance}
                         >
-                            {loading ? 'Airdrop en cours...' : 'AirDrop SOL'}
+                            Check Balance
                         </button>
                     </p>
                 </>
             ) : (
                 <button
-                    style={{
-                        padding: '10px 20px',
-                        backgroundColor: '#008CBA',
-                        color: 'white',
-                        border: 'none',
-                        borderRadius: '5px',
-                        cursor: 'pointer',
-                    }}
+                    type={'button'}
+                    disabled={loading}
                     onClick={connectWallet}
                 >
                     Connecter le portefeuille Phantom
